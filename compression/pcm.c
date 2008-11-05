d462a52ea38be9211d705f89448efda294939223
Xavier Deguillard <deguil_x@epita.fr>
2008-11-05 23:18:36 +0000

    bon la lecture des données est normalement opérationnel

--- compression/pcm.c
+++ compression/pcm.c
@@ -3,39 +3,60 @@
 
 #include "pcm.h"
 
+void read_data(wav_file *wav, FILE *file);
+
 wav_file *open_wav(const char *filename)
 {
-	FILE *wav;
+	FILE *file;
 	wav_file *result;
 
-	wav = fopen(filename, "rb");
-	if (wav == NULL)
+	file = fopen(filename, "rb");
+	if (file == NULL)
 	{
 		printf("file not exist\n");
 		exit(1);
 	}
 	result = malloc(sizeof(wav_file));
 	/* RIFF header */
-	fread(&result->ChunkID, 1, 4 * sizeof(char), wav);
+	fread(&result->ChunkID, 1, 4 * sizeof(char), file);
 	printf("%s\n", result->ChunkID);
-	fread(&result->ChunkSize, 1, sizeof(unsigned int), wav);
-	fread(&result->Format, 1, 4 * sizeof(char), wav);
+	fread(&result->ChunkSize, 1, sizeof(unsigned int), file);
+	fread(&result->Format, 1, 4 * sizeof(char), file);
 	printf("%s\n", result->Format);
 	/* fmt */
-	fread(&result->FormatBlocID, 1, 4 * sizeof(char), wav);
+	fread(&result->FormatBlocID, 1, 4 * sizeof(char), file);
 	printf("%s\n", result->FormatBlocID);
-	fread(&result->FormatBlocSize, 1, sizeof(unsigned int), wav);
-	fread(&result->AudioFormat, 1, sizeof(unsigned short int), wav);
-	fread(&result->NumChannels, 1, sizeof(unsigned short int), wav);
-	fread(&result->SampleRate, 1, sizeof(unsigned int), wav);
-	fread(&result->ByteRate, 1, sizeof(unsigned int), wav);
-	fread(&result->BlockAlign, 1, sizeof(unsigned short int), wav);
-	fread(&result->BitsPerSample, 1, sizeof(unsigned short int), wav);
+	fread(&result->FormatBlocSize, 1, sizeof(unsigned int), file);
+	fread(&result->AudioFormat, 1, sizeof(unsigned short int), file);
+	fread(&result->NumChannels, 1, sizeof(unsigned short int), file);
+	fread(&result->SampleRate, 1, sizeof(unsigned int), file);
+	fread(&result->ByteRate, 1, sizeof(unsigned int), file);
+	fread(&result->BlockAlign, 1, sizeof(unsigned short int), file);
+	fread(&result->BitsPerSample, 1, sizeof(unsigned short int), file);
+	printf("taille d'un échantillon : %u\n", result->BitsPerSample);
 	/* data */
-	fread(&result->DataBlocID, 1, 4 * sizeof(char), wav);
+	fread(&result->DataBlocID, 1, 4 * sizeof(char), file);
 	printf("%s\n", result->DataBlocID);
-	fread(&result->DataBlocSize, 1, sizeof(unsigned int), wav);
+	fread(&result->DataBlocSize, 1, sizeof(unsigned int), file);
+	printf("taille des données : %u\n", result->DataBlocSize+44);
 
-	fclose(wav);
+	read_data(result, file);
+
+	fclose(file);
 	return result;
+}
+
+void read_data(wav_file *wav, FILE *file)
+{
+	int i, j;
+	int size;
+	int sizeofpos;
+	size = ((wav->BitsPerSample / 8) * wav->DataBlocSize / wav->NumChannels);
+	sizeofpos = (wav->BitsPerSample / 8) * sizeof(char);
+	wav->Data = malloc(wav-> NumChannels);
+	for (i = 0; i < wav->NumChannels; i++)
+		wav->Data[i] = malloc(size);
+	for (j = 0; j < size; j++)
+		for (i = 0; i < wav->NumChannels; i++)
+			fread(&wav->Data[i][j], 1, sizeofpos, file);
 }
