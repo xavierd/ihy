c8c83cc076a79cb346e647630239b30b0a4a24c6
Stephane Sezer <sezer_s@epita.fr>
2008-12-17 23:15:44 +0000

    Modification des makefiles, dont celui de la compression pour avoir une règle générique pour les fichiers .o, ajout d'un fichier AUTHORS, modification du TODO, et gestion très simple des paramètres dans le main.c de la compression.

--- Makefile
+++ Makefile
@@ -3,3 +3,8 @@ all:
 #	make -C src/decompression
 #	make -C src/reader
 
+clean:
+	make -C src/compression clean
+#	make -C src/decompression clean
+#	make -C src/reader clean
+
--- src/compression/main.c
+++ src/compression/main.c
@@ -3,10 +3,15 @@
 
 #include "pcm.h"
 
-int main()
+int main(int argc, char ** argv)
 {
-	wav_file *test;
-	test = open_wav("../../misc/wav/test.wav");
-	close_wav(test);
-	return 1;
+	if (argc <= 1) {
+		printf("error: please specify filename\n");
+		return 1;
+	} else {
+		wav_file *test;
+		test = open_wav(argv[1]);
+		close_wav(test);
+		return 0;
+	}
 }
