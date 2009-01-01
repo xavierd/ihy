de4baaf25e00e4ae5388a3781fbff9b870dc16b4
Stephane Sezer <sezer_s@epita.fr>
2009-01-01 12:41:28 +0000

    Quelques modifications dans les makefiles et ajout de fichiers bidons pour les autres parties du projet.

--- src/compression/main.c
+++ src/compression/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "pcm.h"
+#include "ihy.h"
 
 int main(int argc, char ** argv)
 {
---
+++ src/reader/main.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
