8769a7c304aa02da541e0f248e44dfb97a7af0c6
Nicolas Geniteau <nicolas@ordi-nico.localdomain>
2009-02-06 21:08:04 +0100

    Un reverse qui marche pour la moitier des valeurs ...

--- src/cli/wavelets/haar.ml
+++ src/cli/wavelets/haar.ml
@@ -3,7 +3,7 @@ let iof = int_of_float
 
 (*
 (* UNIQUEMENT POUR LES TESTS *)
-
+#load "bigarray.cma"
 
 let showBig a f =
   for i=0 to (Bigarray.Array1.dim a) - 1 do
@@ -23,17 +23,24 @@ let echan signal p bInf bSup =
       x := !x +. p
     done;
     res
-
-let signal = echan (sin) (0.125) (-.1.) (1.)
+      
+let compare t1 t2 =
+  let res = Bigarray.Array1.create (Bigarray.float32)
+    Bigarray.c_layout (Bigarray.Array1.dim t1) in
+    for i = 0 to (Bigarray.Array1.dim t1) - 1 do
+      res.{i} <- t1.{i} -. t2.{i};
+    done;
+    res
 
 let main () =
-  let signal = echan (sin) (0.125) (-.1.) (1.) in
+  let signal = echan (sin) (0.125) (-.2.) (2.) in
     showBig signal print_float;
-    showBig (haar_direct signal) print_float
+    let t = haar_direct signal in
+    let a = haar_reverse t in
+      showBig (compare signal a) print_float
 ;;
 
 main () ;;      
-
 filter_directD signal.{0} signal.{1} ;;
 (* FIN TESTS *)
 *)
@@ -45,6 +52,11 @@ let filter_direct x y =
 let filter_directD x y =
   let coef = (2. ** (-.(1.)/.(2.))) in
     coef *. (y -. x)
+
+let filter_reverse y x op=
+  let coef = (2. ** (-.(1.)/.(2.))) *. 2.  in
+    (op y x) /. coef
+
 
 let haar_direct a =
   let len = Bigarray.Array1.dim a in
@@ -68,5 +80,39 @@ let haar_direct a =
     res.{0} <- a.{0};
     res
 
+let haar_reverse a =
+  let len = Bigarray.Array1.dim a in
+  let nbrRow = iof ((log (foi len)) /. (log 2.)) in
+  let posInit = ref 0 in
+  let t1 = Bigarray.Array1.create (Bigarray.float32)
+    Bigarray.c_layout len in
+  let t2 = Bigarray.Array1.create (Bigarray.float32)
+    Bigarray.c_layout len in
+    t1.{0} <- a.{0};
+    for i=1 to nbrRow do
+      let (prev, next) = 
+	if (i mod 2 = 1) then
+	  (t1, t2)
+	else
+	  (t2, t1)
+      in
+      let step = (iof (2. ** (foi (i-1)))) in
+	posInit := step;
+	let j = ref 0 in
+	  while (!j < (step * 2)) do
+	    next.{!j} <- filter_reverse (prev.{!j/2}) (a.{!posInit}) (-.);
+	    next.{!j + 1} <- filter_reverse (prev.{!j/2}) (a.{!posInit}) (+.);
+	    j := !j + 2;
+	    posInit := !posInit + 1;
+	  done;
+   done;
+    if (nbrRow mod 2 = 1) then
+      t2
+    else
+      t1
+
 let _ =
   Callback.register "Haar_Direct" haar_direct
+
+let _ =
+  Callback.register "Haar_Reverse" haar_reverse
