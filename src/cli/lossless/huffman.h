1c7e427c134dd89fb5b9f5a605b6dc09e39e37c1
Xavier Deguillard <deguil_x@epita.fr>
2009-02-08 00:50:04 +0100

    huffman sait desormais encoder (non teste)

--- src/cli/lossless/huffman.h
+++ src/cli/lossless/huffman.h
@@ -9,6 +9,8 @@ typedef struct huffman_tree
 {
     char			letter;
     int				occurrence;
+    int				code;
+    int				codelength;
     struct huffman_tree		*fg;
     struct huffman_tree		*fd;
 } huffman_tree;
@@ -18,6 +20,12 @@ void huffman_pretty(huffman_tree *B, int code, int code_size);
 
 /* build the huffman tree, and return it */
 huffman_tree *build_huffman(const void *varray, const size_t n);
+
+/* encode the data
+ * n is the size of varray and will be, when the function returns, the
+ * size of the array returned
+ */
+int8_t *huffman_encode(const void *varray, size_t *n, huffman_tree *H);
 
 /* release the memory used by a huffman tree */
 void destroy_huffman(huffman_tree *B);
