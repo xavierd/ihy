a0618171a1ee7259cf3e76ccacaf5107dc6af5ef
Stephane Sezer <sezer_s@epita.fr>
2009-02-08 23:05:17 +0100

    re-commit de ce qui a été perdu

--- src/cli/utils/half.c
+++ src/cli/utils/half.c
@@ -2,12 +2,40 @@
 
 t_half float_to_half(float f)
 {
-    t_half h;
-    uint32_t f2;
+    if (f == 0)
+    {
+	return 0;
+    }
+    else
+    {
+      t_half h;
+      int32_t f2;
+      int32_t s, e, m;
 
-    memcpy(&f2, &f, sizeof(float));
-    printf("%i", f2);
-    h = (f2 >> 31) << 15;
+      memcpy(&f2, &f, sizeof(float));
 
-    return h;
+      s = f2 >> 31;
+      printf("%i\n", s);
+      e = ((f2 << 1) >> 24) - 127;
+      printf("%i\n", e);
+      m = (f2 << 9) >> 9;
+      printf("%i\n", m);
+
+      h = s << 15;
+
+      if (e < 0)
+      {
+	  e = -e;
+	  e = (e << 27) >> 27;
+	  h = h + ((-e + 5) << 10);
+      }
+      else
+      {
+	  e = (e << 27) >> 27;
+	  h = h + (e + 15 << 10);
+      }
+
+      h = h + (m >> 13);
+
+      return h;
 }
