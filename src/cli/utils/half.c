a7bee2a20b3b323b077d3d026ecda01ef5d40ca4
Xavier Deguillard <deguil_x@epita.fr>
2009-02-09 22:46:32 +0100

    il faudrait au moins que cette branch compile half.c...

--- src/cli/utils/half.c
+++ src/cli/utils/half.c
@@ -8,35 +8,35 @@ t_half float_to_half(float f)
     }
     else
     {
-      t_half h;
-      int32_t f2;
-      int32_t s, e, m;
+	t_half h;
+	int32_t f2;
+	int32_t s, e, m;
 
-      memcpy(&f2, &f, sizeof(float));
+	memcpy(&f2, &f, sizeof(float));
 
-      s = f2 >> 31;
-      printf("%i\n", s);
-      e = ((f2 << 1) >> 24) - 127;
-      printf("%i\n", e);
-      m = (f2 << 9) >> 9;
-      printf("%i\n", m);
+	s = f2 >> 31;
+	printf("%i\n", s);
+	e = ((f2 << 1) >> 24) - 127;
+	printf("%i\n", e);
+	m = (f2 << 9) >> 9;
+	printf("%i\n", m);
 
-      h = s << 15;
+	h = s << 15;
 
-      if (e < 0)
-      {
-	  e = -e;
-	  e = (e << 27) >> 27;
-	  h = h + ((-e + 5) << 10);
-      }
-      else
-      {
-	  e = (e << 27) >> 27;
-	  h = h + (e + 15 << 10);
-      }
+	if (e < 0)
+	{
+	    e = -e;
+	    e = (e << 27) >> 27;
+	    h = h + ((-e + 5) << 10);
+	}
+	else
+	{
+	    e = (e << 27) >> 27;
+	    h = h + (e + 15 << 10);
+	}
 
-      h = h + (m >> 13);
+	h = h + (m >> 13);
 
-      return h;
+	return h;
     }
 }
