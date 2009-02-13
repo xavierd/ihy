96a480b7f6caa8ba2fa940442e873f0f0d5d88d5
Xavier Deguillard <deguil_x@epita.fr>
2009-02-13 17:57:46 +0100

    du menage dans huffman ainsi que documentation de l'encodage

--- src/cli/lossless/huffman.h
+++ src/cli/lossless/huffman.h
@@ -17,17 +17,11 @@ typedef struct huffman_tree
     struct huffman_tree		*fd;
 } huffman_tree;
 
-/* pretty print huffman */
-void huffman_pretty(huffman_tree *B, int code, int code_size);
-
-/* build the huffman tree, and return it */
-huffman_tree *build_huffman(const void *varray, const size_t n);
-
 /* encode the data
  * n is the size of varray and will be, when the function returns, the
  * size of the array returned
  */
-int8_t *huffman_encode(const void *varray, size_t *n, huffman_tree *H);
+int8_t *huffman_encode(const void *varray, size_t *n);
 
 /* release the memory used by a huffman tree */
 void destroy_huffman(huffman_tree *B);
