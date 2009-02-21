2db7d390c4b83538b249443a2d3e24760cdc6b6f
Xavier Deguillard <deguil_x@epita.fr>
2009-02-21 17:46:53 +0100

    ajout de fonctions générique pour la lecture audio

--- src/cli/output/ao.h
+++ src/cli/output/ao.h
@@ -5,6 +5,15 @@
 
 #include <input/wav.h>
 
+/* return the device, to play sound in streaming */
+ao_device *init_device(int BitsPerSample, int NumChannels, int SampleRate);
+
+/* close the device, and do some cleaning */
+void close_device(ao_device *device);
+
+/* play the content of array */
+void play(ao_device *device, void *array, int size);
+
 void play_wav(wav_data *wav);
 
 #endif
