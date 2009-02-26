6a694c6a57f77f261a90ce9a76882534e8111ced
Xavier Deguillard <deguil_x@epita.fr>
2009-02-26 12:28:19 +0100

    on verifie que le buffer a déjà en partie été rempli avant de lire.

--- src/cli/audio_output/ihy_streaming.c
+++ src/cli/audio_output/ihy_streaming.c
@@ -4,6 +4,7 @@ struct ihy_streaming_data
 {
     t_buffer	buffer;
     ihy_data	*ihy;
+    int		*has_filled_the_buffer;
 };
 
 struct ihy_buffer_content
@@ -19,14 +20,15 @@ static void *ihy_playing(void *data)
     ihy_data *ihy = ((struct ihy_streaming_data *)data)->ihy;
     ao_device *audio_device;
     struct ihy_buffer_content *to_play;
+    int *can_play = ((struct ihy_streaming_data *)data)->has_filled_the_buffer;
 
     audio_device = ao_init_device(16, ihy->Channels, ihy->Frequency);
 
-    /* comme pour le wav, il se peut que le premier thread n'ait
-     * pas encore commencé a décompresser, on pourra rajouter un
-     * peu d'attente (100ms devrait suffire)
+    /* we need to ensure that the other thread has added,
+     * at least, 1 element
      */
-    sleep(1);
+    while (!*can_play)
+	usleep(50);
     while (!buffer_isempty(buffer))
     {
 	to_play = buffer_get(buffer);
@@ -57,6 +59,8 @@ static void *ihy_filling_buffer(void *data)
 			 0);
 	to_add->samplesSize = to_add->samplesSize / 2;
 	buffer_add(to_add, buffer);
+	if (!i) /* i == 0 */
+	    *(((struct ihy_streaming_data *)data)->has_filled_the_buffer) = 1;
 	i++;
     }
     return NULL;
@@ -67,10 +71,12 @@ void play_ihy_streaming(ihy_data *ihy)
     t_buffer buffer; /* contains ihy_buffer_content */
     pthread_t playing_thread, filling_buffer_thread;
     struct ihy_streaming_data data;
+    int has_filled_the_buffer = 0;
 
     buffer = buffer_init(10); /* max 10 elements */
     data.ihy = ihy;
     data.buffer = buffer;
+    data.has_filled_the_buffer = &has_filled_the_buffer;
     pthread_create(&filling_buffer_thread, NULL, &ihy_filling_buffer, &data);
     pthread_create(&playing_thread, NULL, &ihy_playing, &data);
     pthread_join(filling_buffer_thread, NULL);
