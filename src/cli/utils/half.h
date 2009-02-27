2b9860e574634585c74b94cbf6232006bff15cba
Stephane Sezer <sezer_s@epita.fr>
2009-02-28 00:25:26 +0100

    En theorie le half est termine.

--- src/cli/utils/half.h
+++ src/cli/utils/half.h
@@ -1,14 +1,25 @@
 #ifndef DEF_HALF
 #define DEF_HALF
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <string.h>
 
-typedef uint16_t t_half;
+struct s_half
+{
+    unsigned int mantissa:10;
+    unsigned int exponent:5;
+    unsigned int negative:1;
+};
+struct s_float
+{
+    unsigned int mantissa:23;
+    unsigned int exponent:8;
+    unsigned int negative:1;
+};
 
-t_half float_to_half(float f);
-float half_to_float(t_half h);
+typedef struct s_half t_half;
+typedef struct s_float t_float;
+
+t_half float_to_half(t_float f);
+t_float half_to_float(t_half h);
 
 #endif
