703f6b0eaf7530bd965de707df3e6e723be8d507
Xavier Deguillard <deguil_x@epita.fr>
2009-02-27 15:34:44 +0100

    un truc moins moche

--- src/cli/audio_output/ihy_streaming.c
+++ src/cli/audio_output/ihy_streaming.c
@@ -4,7 +4,6 @@ struct ihy_streaming_data
 {
     t_buffer	buffer;
     ihy_data	*ihy;
-    int		*has_filled_the_buffer;
 };
 
 struct ihy_buffer_content
@@ -20,14 +19,13 @@ static void *ihy_playing(void *data)
     ihy_data *ihy = ((struct ihy_streaming_data *)data)->ihy;
     ao_device *audio_device;
     struct ihy_buffer_content *to_play;
-    int *can_play = ((struct ihy_streaming_data *)data)->has_filled_the_buffer;
 
     audio_device = ao_init_device(16, ihy->Channels, ihy->Frequency);
 
     /* we need to ensure that the other thread has added,
      * at least, 1 element
      */
-    while (!*can_play)
+    while (buffer_isempty(buffer))
 	usleep(50);
     while (!buffer_isempty(buffer))
     {
@@ -59,8 +57,6 @@ static void *ihy_filling_buffer(void *data)
 			 0);
 	to_add->samplesSize = to_add->samplesSize / 2;
 	buffer_add(to_add, buffer);
-	if (!i) /* i == 0 */
-	    *(((struct ihy_streaming_data *)data)->has_filled_the_buffer) = 1;
 	i++;
     }
     return NULL;
@@ -71,12 +67,10 @@ void play_ihy_streaming(ihy_data *ihy)
     t_buffer buffer; /* contains ihy_buffer_content */
     pthread_t playing_thread, filling_buffer_thread;
     struct ihy_streaming_data data;
-    int has_filled_the_buffer = 0;
 
     buffer = buffer_init(10); /* max 10 elements */
     data.ihy = ihy;
     data.buffer = buffer;
-    data.has_filled_the_buffer = &has_filled_the_buffer;
     pthread_create(&filling_buffer_thread, NULL, &ihy_filling_buffer, &data);
     pthread_create(&playing_thread, NULL, &ihy_playing, &data);
     pthread_join(filling_buffer_thread, NULL);
