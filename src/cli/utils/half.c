c259d8da2e5beff3037afae3d38657738ea3368d
Stephane Sezer <sezer_s@epita.fr>
2009-02-28 00:25:26 +0100

    En theorie le half est termine.

--- src/cli/utils/half.c
+++ src/cli/utils/half.c
@@ -1,62 +1,23 @@
 #include "half.h"
 
-t_half float_to_half(float f)
+t_half float_to_half(t_float f)
 {
-    if (f == 0)
-    {
-	return 0;
-    }
-    else
-    {
-	t_half h;
-	int32_t f2;
-	int32_t s, e, m;
+    t_half h;
 
-	memcpy(&f2, &f, sizeof(float));
+    h.negative = f.negative;
+    h.exponent = f.exponent;
+    h.mantissa = f.mantissa >> 13;
 
-	s = f2 >> 31;
-	e = ((f2 << 1) >> 24) - 127;
-	m = (f2 << 9) >> 9;
-
-	h = s << 15;
-
-	if (e < 0)
-	{
-	    e = -e;
-	    e = (e << 27) >> 27;
-	    h = h + ((-e + 5) << 10);
-	}
-	else
-	{
-	    e = (e << 27) >> 27;
-	    h = h + (e + 15 << 10);
-	}
-
-	h = h + (m >> 13);
-
-	return h;
-    }
+    return h;
 }
 
-float half_to_float(t_half h)
+t_float half_to_float(t_half h)
 {
-    if (h == 0)
-    {
-	return 0.;
-    }
-    else
-    {
-	float f;
-	int32_t s, e, m;
+    t_float f;
 
-	s = h >> 15;
-	e = ((h << 1) >> 10) - 15;
-	m = (h << 6) >> 6;
+    f.negative = h.negative;
+    f.exponent = h.exponent;
+    f.mantissa = h.mantissa << 13;
 
-	f = s << 31;
-	f = h + ((e + 127) << 23);
-	f = h + (m << 13);
-
-	return f;
-    }
+    return f;
 }
