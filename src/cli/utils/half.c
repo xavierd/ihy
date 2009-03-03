6b15b2c0cff52b9df550fdf0dc6637b940c6836d
Stephane Sezer <sezer_s@epita.fr>
2009-03-03 11:34:00 +0100

    Ajout d'une des fonctions de conversion pour les tableaux/halfs.

--- src/cli/utils/half.c
+++ src/cli/utils/half.c
@@ -1,5 +1,31 @@
 #include "half.h"
 
+struct s_half
+{
+    unsigned int mantissa:10;
+    unsigned int exponent:5;
+    unsigned int negative:1;
+};
+typedef union u_half t_half;
+union u_half
+{
+    uint16_t h;
+    struct s_half ieee;
+};
+
+struct s_float
+{
+    unsigned int mantissa:23;
+    unsigned int exponent:8;
+    unsigned int negative:1;
+};
+typedef union u_float t_float;
+union u_float
+{
+    float f;
+    struct s_float ieee;
+};
+
 uint16_t float_to_half(float f)
 {
     if (f == 0.)
@@ -39,3 +65,23 @@ float half_to_float(uint16_t h)
 	return f.f;
     }
 }
+
+uint16_t* floatarray_to_half(float *fa, size_t size)
+{
+    uint16_t *res;
+    int i;
+
+    res = malloc(size * sizeof(uint16_t));
+    for (i = 0; i < size; i++)
+    {
+	res[i] = float_to_half(fa[i]);
+    }
+
+    return res;
+}
+
+/*
+float* halfarray_to_float(float *fa, size_t size)
+{
+}
+*/
