944457e5c2252d68afb85eece6e5541a35bd52b1
Xavier Deguillard <deguil_x@epita.fr>
2009-03-18 21:51:27 +0100

    la fin de la musique ne contient plus de random

--- src/cli/compression/wavelet.h
+++ src/cli/compression/wavelet.h
@@ -17,9 +17,13 @@ int get_nbChunk(const int chunk_size, const int nb);
 
 /* compute the result of the OCaml function "Haar_Direct"
  * compress the data and fill out
+ * assuming dim = 2^n
+ * real_size is quite particular, it is here to avoid getting values behind the
+ * end of samples (should only happen on the last chunk)
  */
 void wavelets_direct(const int8_t *samples,
 		     const size_t dim, /* size of prec arg */
+		     size_t real_size,
 		     const size_t sampleSize, /* in bytes */
 		     const uint16_t nbChannels,
 		     float *out);
