8d3e96f348e69938740947e74cc5a61d2eeeee8d
Xavier Deguillard <deguil_x@epita.fr>
2009-03-21 17:10:47 +0100

    putain ca maaarche

--- src/cli/audio_output/ao.c
+++ src/cli/audio_output/ao.c
@@ -1,38 +1,14 @@
 #include "ao.h"
 
-/* Pseudo-Code :
- * init_device :
- *	create the buffers
- *	return the device
- * add_samples :
- *	just add it to the next pos
- * Callback :
- *	just get the next pos
- */
-
-#if 0
-void ao_play_samples(ao_device *device, void *array, int size)
-{
-    device->framecount = size;
-}
-
-void ao_close_device(ao_device *device)
-{
-    AudioQueueDispose(device->queue, true);
-    free(device);
-}
-#endif
-
 static void Callback(void *in, AudioQueueRef inQ, AudioQueueBufferRef out)
 {
     ao_device *dev = (ao_device *)in;
     short *audioBuffer = (short *)out->mAudioData;
 
-    printf("callback, %d, %d\n", dev->Chunk, dev->NbChunk(dev->Data));
     if (dev->Chunk <= dev->NbChunk(dev->Data))
     {
 	dev->DecodeFunction(dev->Data, dev->Chunk, audioBuffer);
-	out->mAudioDataByteSize = 65536;
+	out->mAudioDataByteSize = 65536 * 2;
 	AudioQueueEnqueueBuffer(inQ, out, 0, NULL);
 	dev->Chunk++;
     }
@@ -46,7 +22,6 @@ static void Callback(void *in, AudioQueueRef inQ, AudioQueueBufferRef out)
 ao_device *ao_init_device(int BitsPerSample, int NumChannels, int SampleRate)
 {
     ao_device *in;
-    int i;
 
     in = malloc(sizeof(ao_device));
     in->mDataFormat.mSampleRate = SampleRate;
@@ -71,7 +46,6 @@ void ao_play(ao_device *device)
     AudioQueueNewOutput(&device->mDataFormat, Callback, device,
 	    CFRunLoopGetCurrent(), kCFRunLoopCommonModes, 0, &device->queue);
 
-    printf("ao_play\n");
     device->isPlaying = true;
     bufferBytes = device->mDataFormat.mBitsPerChannel * 65536 / 8;
     for (i = 0; i < NBBUFFERS; i++)
@@ -83,12 +57,11 @@ void ao_play(ao_device *device)
     AudioQueueSetParameter(device->queue, kAudioQueueParam_Volume, 1.0);
 
     AudioQueueStart(device->queue, NULL);
-    printf("start\n");
     while (device->isPlaying)
     {
 	CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0.25, false);
     }
 
-    AudioQueueDispose(device->queue, true);
+    AudioQueueDispose(device->queue, false);
     free(device);
 }
--- src/cli/audio_output/ihy_streaming.c
+++ src/cli/audio_output/ihy_streaming.c
@@ -1,77 +1,5 @@
 #include "ihy_streaming.h"
 
-#if 0
-struct ihy_streaming_data
-{
-    t_buffer	buffer;
-    ihy_data	*ihy;
-};
-
-struct ihy_buffer_content
-{
-    int8_t	*samples;
-    uint32_t	samplesSize;
-};
-
-static void *ihy_playing(void *data)
-{
-    /* contains uncompressed data, (ihy_buffer_content *) */
-    t_buffer buffer = ((struct ihy_streaming_data *)data)->buffer;
-    ihy_data *ihy = ((struct ihy_streaming_data *)data)->ihy;
-    ao_device *audio_device;
-    struct ihy_buffer_content *to_play;
-
-    audio_device = ao_init_device(16, ihy->Channels, ihy->Frequency);
-
-    /* we need to ensure that the other thread has added,
-     * at least, 1 element
-     */
-    while (buffer_isempty(buffer))
-	usleep(50);
-    while (!buffer_isempty(buffer))
-    {
-	to_play = buffer_get(buffer);
-	ao_play_samples(audio_device, to_play->samples, to_play->samplesSize);
-	free(to_play); /* hum quite dangerous */
-    }
-    ao_close_device(audio_device);
-    return NULL;
-}
-
-static void *ihy_filling_buffer(void *data)
-{
-    /* contains uncompressed data, (ihy_buffer_content *) */
-    t_buffer buffer = ((struct ihy_streaming_data *)data)->buffer;
-    ihy_data *ihy = ((struct ihy_streaming_data *)data)->ihy;
-    struct ihy_buffer_content *to_add;
-    unsigned int i = 0;
-    void *oldValues;
-
-    while (i < ihy->NbChunk)
-    {
-	to_add = malloc(sizeof(struct ihy_buffer_content));
-	to_add->samplesSize = ihy->DataChunks[i].ChunkSize;
-	to_add->samples =
-	    huffman_decode(ihy->DataChunks[i].Values, &to_add->samplesSize);
-	oldValues = to_add->samples;
-	to_add->samples = (int8_t *)halfarray_to_float(
-		(uint16_t *)to_add->samples,
-		to_add->samplesSize / sizeof(uint16_t));
-	free(oldValues);
-	to_add->samplesSize *= 2;
-	wavelets_inverse((float *)to_add->samples,
-			 to_add->samplesSize / sizeof(float),
-			 ihy->Channels,
-			 to_add->samples,
-			 0);
-	to_add->samplesSize = to_add->samplesSize / 2; /*samplesize = 16bits*/
-	buffer_add(to_add, buffer);
-	i++;
-    }
-    return NULL;
-}
-
-#endif
 static int get_nb_chunk(void *Data)
 {
     ihy_data *ihy = Data;
@@ -82,8 +10,8 @@ static int get_nb_chunk(void *Data)
 static void DecodeIhy(void *Data, int chunk, void *out)
 {
     ihy_data *ihy = Data;
-    int8_t *toDecode = ihy->DataChunks[chunk].Values;
-    int8_t *Values, *oldValues;
+    uint8_t *toDecode = ihy->DataChunks[chunk].Values;
+    uint8_t *Values, *oldValues;
     int size = ihy->DataChunks[chunk].ChunkSize;
 
     Values = huffman_decode(toDecode, &size);
@@ -101,10 +29,8 @@ void play_ihy_streaming(ihy_data *ihy)
 
     dev = ao_init_device(16, ihy->Channels, ihy->Frequency);
     dev->Data = ihy;
-    printf("init\n");
     dev->NbChunk = &get_nb_chunk;
     dev->DecodeFunction = &DecodeIhy;
 
-    printf("play\n");
     ao_play(dev);
 }
