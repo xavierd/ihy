82addcf222f2736f48b3b3f301996c09eee78ab8
Xavier Deguillard <deguil_x@epita.fr>
2009-05-04 22:03:25 +0200

    l'écriture "parait" bonne

--- src/cli/compression/quantization.c
+++ src/cli/compression/quantization.c
@@ -22,17 +22,18 @@ static int nb_bits(int a)
 }
 
 #define ith_bit(number, i)		\
-    (((number) >> ((i) - 1)) & 1
+    (((number) >> ((i) - 1)) & 1)
 
-static void *encode(int *x, const size_t n, const int nb_bits, int *size)
+static void *encode(int *x, const size_t n, const int nb_bits, size_t *size)
 {
     void *res;
     char *tab;
-    int i, j;
+    size_t i;
+    int j;
     char buf;
     int offset;
 
-    *size = 1 + ((nb_bits * n) / 8);
+    *size = /*1 +*/ ((nb_bits * n) / 8);
     res = malloc(*size);
     tab = res;
     offset = 0;
@@ -54,23 +55,28 @@ static void *encode(int *x, const size_t n, const int nb_bits, int *size)
     return res;
 }
 
-void quantizate(float *x, const size_t n, const float factor)
+void *quantizate(float *x, size_t *n, const float factor)
 {
     size_t i;
     int min, max;
+    void *res;
+    int *q;
 
-    for (i = 0; i < n; i++)
+    q = malloc(*n * sizeof(int));
+    min = max = 0;
+    for (i = 0; i < *n; i++)
     {
-	x[i] = sign(x[i]) * floorf(abs(x[i] / factor));
-	if (x[i] > max)
-	    max = x[i];
-	else if (x[i] < min)
-	    min = x[i];
+	q[i] = sign(x[i]) * floorf(abs(x[i] / factor));
+	if (q[i] > max)
+	    max = q[i];
+	else if (q[i] < min)
+	    min = q[i];
     }
     min = abs(min);
     max = max > min ? max : min;
     min = nb_bits(max);
-    printf("%d\n", min);
+    res = encode(q, *n, min, n);
+    return res;
 }
 
 void dequantizate(float *x, const size_t n, const float factor)
