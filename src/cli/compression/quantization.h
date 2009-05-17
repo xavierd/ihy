91af0256d445688d58a38a4297635882aae7c203
Xavier Deguillard <deguil_x@epita.fr>
2009-05-17 21:17:28 +0200

    quelques modifs pour que ca compile

--- src/cli/compression/quantization.h
+++ src/cli/compression/quantization.h
@@ -2,9 +2,10 @@
 #define DEF_QUANTIZATION
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <math.h>
 
 void *quantizate(float *x, size_t *n, const float factor, int *nbbits);
-float *dequantizate(float *x, size_t *n, const float factor, int nbbits);
+float *dequantizate(uint8_t *x, size_t *n, const float factor, int nbbits);
 
 #endif
