05d6ccddf757cefea8198d10ccfef7cfd30cc9f5
Xavier Deguillard <deguil_x@epita.fr>
2009-05-20 16:40:50 +0200

    ajout d'une fonction de décompression de chunk générique

---
+++ src/cli/compression/ihy.c
@@ -0,0 +1,23 @@
+#include "ihy.h"
+
+void uncompress_chunk(ihy_chunk *chunk, int8_t *samples, int ch)
+{
+    size_t size;
+    void *tmp;
+
+    /* Huffman */
+    tmp = chunk->Values;
+    chunk->ChunkSize = chunk->HUncompressedSize;
+    chunk->Values = huffman_decode(tmp, chunk->ChunkSize);
+    free(tmp);
+    /* End Huffman */
+    /* Quantification */
+    size = chunk->ChunkSize;
+    tmp = chunk->Values;
+    tmp = dequantizate(tmp, &size, chunk->QScaleFactor, chunk->QBitsPerCoefs);
+    free(chunk->Values);
+    /* End Quantification */
+    /* Wavelets */
+    wavelets_inverse(tmp, size, ch, samples, 0);
+    /* End Wavelets */
+}
