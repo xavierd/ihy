d058d011c4ed08bddec8d77ce8fbe7e039181cbb
Xavier Deguillard <deguil_x@epita.fr>
2009-05-21 15:25:49 +0200

    fonction de compression d'un chunk

--- src/cli/compression/ihy.c
+++ src/cli/compression/ihy.c
@@ -22,3 +22,36 @@ void uncompress_chunk(ihy_chunk *chunk, int8_t *samples, int channels)
     wavelets_inverse(tmp, size, channels, samples);
     /* End Wavelets */
 }
+
+/*
+ * Assuming chunk->ChunkSize == ihy->ChunkSize
+ */
+void compress_chunk(int8_t *samples, size_t size, uint16_t ch, ihy_chunk *chunk)
+{
+    int8_t *pow2_samples;
+    void *tmp, *oldValue;
+    int nbbits;
+
+    pow2_samples = calloc(chunk->ChunkSize * 2, 1);
+    memcpy(pow2_samples, samples, size);
+    chunk->Values = malloc((chunk->ChunkSize / 2) * sizeof(float));
+    size = chunk->ChunkSize * 2;
+    wavelets_direct(pow2_samples, size, size, ch, chunk->Values);
+    free(pow2_samples);
+    chunk->ChunkSize = (chunk->ChunkSize / 2) * sizeof(float);
+    do
+    {
+	oldValue = chunk->Values;
+	size = chunk->ChunkSize / sizeof(float);
+	oldValue = quantizate(oldValue, &size, 200.0f, &nbbits);
+	tmp = oldValue;
+	chunk->HUncompressedSize = size;
+	oldValue = huffman_encode(tmp, &size);
+	free(tmp);
+    }
+    while (!size);
+    chunk->QBitsPerCoefs = nbbits;
+    chunk->QScaleFactor = 200.0f;
+    chunk->ChunkSize = size;
+    chunk->Values = oldValue;
+}
--- src/cli/compression/ihy.h
+++ src/cli/compression/ihy.h
@@ -1,6 +1,7 @@
 #ifndef DEF_IHY_COMPRESSION
 #define DEF_IHY_COMPRESSION
 
+#include <string.h>
 #include <codecs/ihy.h>
 #include <compression/huffman.h>
 #include <compression/quantization.h>
@@ -11,5 +12,7 @@
  * samples should be alloced and had a sufficient size
  */
 void uncompress_chunk(ihy_chunk *chunk, int8_t *samples, int channels);
+
+void compress_chunk(int8_t *samples, size_t size, uint16_t ch, ihy_chunk *chunk);
 
 #endif
